dd4017dc359986025b31bdc2e27782d91c5fadf1
shkryl <shkryl@rambler.ru>
2025-08-08 20:21:55 +0400

    Made solution for task 3->lengthOfLongestSubstring from leetcode.

---
+++ src/main/java/info/shkryl/task3_LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,36 @@
+package info.shkryl.task3_LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            while (set.contains(currentChar)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(currentChar);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb")); // 3
+        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));    // 1
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));   // 3
+        System.out.println(solution.lengthOfLongestSubstring(""));         // 0
+        System.out.println(solution.lengthOfLongestSubstring("abba"));     // 2
+    }
+}
