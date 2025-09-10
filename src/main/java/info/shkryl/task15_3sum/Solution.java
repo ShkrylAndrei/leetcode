1aa0ac2d365305fa8b17f8de945aab84db7a2556
shkryl <shkryl@rambler.ru>
2025-09-10 09:50:04 +0400

    Made solution for task 15->3Sum from leetcode.

---
+++ src/main/java/info/shkryl/task15_3sum/Solution.java
@@ -0,0 +1,49 @@
+package info.shkryl.task15_3sum;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+
+        Arrays.sort(nums); // Сортируем для two pointers
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Пропускаем дубликаты первого элемента
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Пропускаем дубликаты для left и right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.threeSum(nums);
+        System.out.println(result);
+    }
+}
